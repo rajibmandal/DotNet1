b3bff314ca3180704dbd219c076feda706d19159
rajibmandal <rajib.mandal.email@gmail.com>
2017-02-28 14:05:09 -0600

    Cor enabled

--- WebAPI/WebAPI/App_Start/WebApiConfig.cs
+++ WebAPI/WebAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebAPI
 {
@@ -14,6 +15,8 @@ namespace WebAPI
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            var cors = new EnableCorsAttribute("*","*","*");
+            config.EnableCors(cors);
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
